05128339dd0aa7a597b1f6faad8e6229d226ea21
bhqyy <naufalbaihaqy1502@gmail.com>
2025-04-18 22:29:44 +0700

    refactoring in setMonthlySalary

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -52,22 +52,16 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	public void setMonthlySalary(int grade) {
+		switch (grade) {
+			case 1 -> monthlySalary = SALARY_GRADE_1;
+			case 2 -> monthlySalary = SALARY_GRADE_2;
+			case 3 -> monthlySalary = SALARY_GRADE_3;
+			default -> throw new IllegalArgumentException("Invalid grade: " + grade);
+		}
+
+		if (isForeigner) {
+			monthlySalary *= FOREIGNER_MULTIPLIER;
 		}
 	}
 	
